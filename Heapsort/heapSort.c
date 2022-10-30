441ab304592ff8cf2d8d48585fda6c672fa6d65c
Paula <paulactalim@gmail.com>
2022-10-30 17:39:58 -0300

    adicao de comentarios

--- Heapsort/heapSort.c
+++ Heapsort/heapSort.c
@@ -3,17 +3,22 @@
 #include <time.h>
 
 /*
-	 * Descricao: essa funcao troca dois elementos de um vetor double
-	 * Parametros: um vetor double (vetor que tera os 
-	 * elementos trocados) e dois inteiros (indice dos 
-	 * dois elementos a serem trocados)
-	 */
+* Descricao: essa funcao troca dois elementos de um vetor
+* Parametros: um vetor de inteiros (vetor que tera os 
+* elementos trocados) e dois inteiros (indice dos 
+* dois elementos a serem trocados)
+*/
 void swap (int vet[], int i, int j) {
     int aux = vet[i];
     vet[i] = vet[j];
     vet[j] = aux;
 }
 
+/* 
+* Descricao: essa funcao calcula o indice o elemento pai no heap
+* Parametro: um inteiro (indice do elemento filho)
+* Retorno: um inteiro (indice do elemento pai)
+*/
 int indice_pai (int filho) {
     int flag = filho;
 
@@ -25,6 +30,12 @@ int indice_pai (int filho) {
     return filho;
 }
 
+/* 
+* Descricao: essa funcao constroi o heap de um vetor de inteiros
+* 
+* Parametros: um vetor de inteiros (vetor a ser construido 
+* a arvore heap) e um inteiro (tamanho valido do vetor)
+*/
 void construir (int vet[], int tam) {
     int indice = indice_pai(tam);
     int i = tam;
@@ -42,6 +53,13 @@ void construir (int vet[], int tam) {
     }
 }
 
+/*
+* Descricao: essa funcao reconstroi um vetor 
+* de numeros inteiros com a arvore heap ja construida 
+* 
+* Parametros: vetor de inteiros (vetor a ser reconstruido) 
+* e um inteiro (tamanho valido do vetor)
+*/
 void reconstruir (int vet[], int tam) {
     int filho;
     int meio = indice_pai(tam);
@@ -50,13 +68,13 @@ void reconstruir (int vet[], int tam) {
     //Verifica se ainda possui filhos
     while(i <= meio) {
         //Seleciona o maior filho
-        if (2*i+1 == tam || vet[2*i+1] > vet[2*i+2]) {
-            filho = 2*i+1;
+        if (2 * i + 1 == tam || vet[2 * i + 1] > vet[2 * i + 2]) {
+            filho = 2 * i + 1;
         } else {
-            filho = 2*i + 2;
+            filho = 2 * i + 2;
         }
 
-        //reconstroi o vetor
+        //Reconstroi o vetor
         if (vet[i] < vet[filho]) {
             swap (vet, i, filho);
             i = filho;
@@ -66,6 +84,12 @@ void reconstruir (int vet[], int tam) {
     }
 }
 
+/* 
+* Descricao: essa funcao ordena um vetor com numeros 
+* inteiros em ordem crescente com o metodo HeapSort
+* 
+* Parametro: um vetor de inteiros (vetor a ser ordenado)
+*/
 void heapSort (int vet[], int n) {
     int tam;
 
