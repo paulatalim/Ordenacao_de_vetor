c434f733411bd3ed976f1024708ee199a433c8d0
Paula <paulactalim@gmail.com>
2022-12-18 13:49:22 -0300

    criando arquivos descrecentes

---
+++ Bubblesort/c/bubbleDoubleDecrescente.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+Descricao: esse procedimento troca dois elementos de um vetor double
+Parametros: um vetor double (vetor que tera os 
+elementos trocados) e dois inteiros (indice dos 
+dois elementos a serem trocados)
+*/
+void swap (double vet[], int i, int j) {
+    double aux = vet[i];
+    vet[i] = vet[j];
+    vet[j] = aux;
+}
+
+/* 
+Descricao: esse procedimento ordena um vetor double
+em ordem crescente com o metodo bubblesort
+
+Parametro: um vetor double (vetor a ser ordenado) e
+um inteiro (tamanho do vetor)
+*/
+void bubblesort (double vet[], int n) {
+    for (int i = n-1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (vet[j] > vet[j+1]) {
+                swap(vet, j, j+1);
+            }
+        }
+    }
+}
+
---
+++ Bubblesort/c/bubbleFloatDecrescente.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+Descricao: esse procedimento troca dois elementos de um vetor de reais
+Parametros: um vetor de inteiros (vetor que tera os 
+elementos trocados) e dois inteiros (indice dos 
+dois elementos a serem trocados)
+*/
+void swap (float vet[], int i, int j) {
+    float aux = vet[i];
+    vet[i] = vet[j];
+    vet[j] = aux;
+}
+
+/* 
+Descricao: esse procedimento ordena um vetor de reais
+em ordem crescente com o metodo bubblesort
+
+Parametro: um vetor de reais (vetor a ser ordenado) e
+um inteiro (tamanho do vetor)
+*/
+void bubblesort (float vet[], int n) {
+    for (int i = n-1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (vet[j] > vet[j+1]) {
+                swap(vet, j, j+1);
+            }
+        }
+    }
+}
+
