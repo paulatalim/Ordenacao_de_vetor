cd714cd2b4fa881bebca2f27e73e386ded09919d
Paula <paulactalim@gmail.com>
2022-12-18 14:02:30 -0300

    adaptando codigos

--- Bubblesort/cpp/bubbleDoubleCrescente.cpp
+++ Bubblesort/cpp/bubbleDoubleCrescente.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 
 /*
-Descricao: esse procedimento troca dois elementos de um vetor de inteiros
+Descricao: esse procedimento troca dois elementos de um vetor double
 
-Parametros: um vetor de inteiros (vetor que tera os 
+Parametros: um vetor double (vetor que tera os 
 elementos trocados) e dois inteiros (indice dos 
 dois elementos a serem trocados)
 */
-void swap (int vet[], int i, int j) {
-    int aux = vet[i];
+void swap (double vet[], int i, int j) {
+    double aux = vet[i];
     vet[i] = vet[j];
     vet[j] = aux;
 }
 
 /* 
-Descricao: esse procedimento ordena um vetor de inteiros
+Descricao: esse procedimento ordena um vetor double
 em ordem crescente com o metodo bubblesort
 
-Parametro: um vetor de inteiros (vetor a ser ordenado) 
+Parametro: um vetor double (vetor a ser ordenado) 
 e um inteiro (tamanho do vetor)
 */
-void bubblesort (int vet[], int n) {
+void bubblesort (double vet[], int n) {
     for (int i = n-1; i > 0; i--) {
         for (int j = 0; j < i; j++) {
             if (vet[j] > vet[j+1]) {
--- Bubblesort/cpp/bubbleFloatCrescente.cpp
+++ Bubblesort/cpp/bubbleFloatCrescente.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 
 /*
-Descricao: esse procedimento troca dois elementos de um vetor de inteiros
+Descricao: esse procedimento troca dois elementos de um vetor de reais
 
-Parametros: um vetor de inteiros (vetor que tera os 
+Parametros: um vetor de reais (vetor que tera os 
 elementos trocados) e dois inteiros (indice dos 
 dois elementos a serem trocados)
 */
-void swap (int vet[], int i, int j) {
-    int aux = vet[i];
+void swap (float vet[], int i, int j) {
+    float aux = vet[i];
     vet[i] = vet[j];
     vet[j] = aux;
 }
 
 /* 
-Descricao: esse procedimento ordena um vetor de inteiros
+Descricao: esse procedimento ordena um vetor de reais
 em ordem crescente com o metodo bubblesort
 
-Parametro: um vetor de inteiros (vetor a ser ordenado) 
+Parametro: um vetor de reais (vetor a ser ordenado) 
 e um inteiro (tamanho do vetor)
 */
-void bubblesort (int vet[], int n) {
+void bubblesort (float vet[], int n) {
     for (int i = n-1; i > 0; i--) {
         for (int j = 0; j < i; j++) {
             if (vet[j] > vet[j+1]) {
