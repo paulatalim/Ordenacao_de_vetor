0259ace0641efaac97238a0baaeda5339793d4c0
Paula <paulactalim@gmail.com>
2022-12-18 13:50:58 -0300

    adaptando os codigos

--- Bubblesort/c/bubbleCharDecrescente.c
+++ Bubblesort/c/bubbleCharDecrescente.c
@@ -15,7 +15,7 @@ void swap (char vet[], int i, int j) {
 
 /* 
 Descricao: esse procedimento ordena um vetor de caracteres
-em ordem alfabetica com o metodo bubblesort
+em ordem decrescente com o metodo bubblesort
 
 Parametro: um vetor de caracteres (vetor a ser ordenado) e
 um inteiro (tamanho do vetor)
@@ -23,7 +23,7 @@ um inteiro (tamanho do vetor)
 void bubblesort (char vet[], int n) {
     for (int i = n-1; i > 0; i--) {
         for (int j = 0; j < i; j++) {
-            if (vet[j] > vet[j+1]) {
+            if (vet[j] < vet[j+1]) {
                 swap(vet, j, j+1);
             }
         }
