1d4d2cfc51f839b0173ea072c2d2e0a76a581746
Paula <paulactalim@gmail.com>
2022-12-18 13:37:49 -0300

    trocando tipos de variaveis e retirada de erro

--- Bubblesort/c/bubbleCharCrescente.c
+++ Bubblesort/c/bubbleCharCrescente.c
@@ -2,28 +2,25 @@
 #include <stdio.h>
 
 /*
-Descricao: esse procedimento troca dois elementos de um vetor de inteiros
-Parametros: um vetor de inteiros (vetor que tera os 
+Descricao: esse procedimento troca dois elementos de um vetor de caracteres
+Parametros: um vetor de caracteres (vetor que tera os 
 elementos trocados) e dois inteiros (indice dos 
 dois elementos a serem trocados)
 */
-void swap (float vet[], int i, int j) {
-    int aux = vet[i];
+void swap (char vet[], int i, int j) {
+    char aux = vet[i];
     vet[i] = vet[j];
     vet[j] = aux;
 }
 
 /* 
-Descricao: esse procedimento ordena um vetor de inteiros
-em ordem crescente com o metodo bubblesort
+Descricao: esse procedimento ordena um vetor de caracteres
+em ordem alfabetica com o metodo bubblesort
 
-Parametro: um vetor de inteiros (vetor a ser ordenado)
+Parametro: um vetor de caracteres (vetor a ser ordenado) e
+um inteiro (tamanho do vetor)
 */
-void bubblesort (int vet[]) {
-    //Calculo do tamanho do vetor
-    int n = sizeof(vet)/sizeof(int);
-
-    //Ordenaacao do vetor
+void bubblesort (char vet[], int n) {
     for (int i = n-1; i > 0; i--) {
         for (int j = 0; j < i; j++) {
             if (vet[j] > vet[j+1]) {
