193abe488579f501fd03aa3d3d6cf2e416e92e60
Paula <paulactalim@gmail.com>
2022-12-19 18:05:14 -0300

    criacao de codigos cpp

---
+++ Insertionsort/cpp/insertionDoubleDecrescente.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+Descricao: essa funcao ordena um vetor double
+em ordem decrescente com o metodo insertionsort
+
+Parametro: um vetor double (vetor a ser ordenado)
+e um inteiro (tamanho do vetor)
+*/
+void insertionsort (double vet[], int n) {
+	for (int i = 1; i < n; i++) {
+		double temp = vet[i];
+		int j = i - 1;
+
+		while (j>=0 && vet[j] < temp) {
+			vet[j+1] = vet[j];
+			j--;
+		}
+
+		vet[j+1] = temp;
+	}
+}
+
