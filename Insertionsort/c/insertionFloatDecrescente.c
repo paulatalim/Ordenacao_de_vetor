fb57b5381c3ee83a69295d3b102a58b64eb58eaf
Paula <paulactalim@gmail.com>
2022-12-19 17:40:36 -0300

    criacao de arquivo para ordenacao decrescente

---
+++ Insertionsort/c/insertionFloatDecrescente.c
@@ -0,0 +1,21 @@
+/* 
+Descricao: essa funcao ordena um vetor de reais
+em ordem crescente com o metodo insertionsort
+
+Parametro: um vetor de reais (vetor a ser ordenado)
+e um inteiro (tamanho do vetor)
+*/
+void insertionsort (float vet[], int n) {
+	for (int i = 1; i < n; i++) {
+		float temp = vet[i];
+		int j = i - 1;
+
+		while (j>=0 && vet[j] > temp) {
+			vet[j+1] = vet[j];
+			j--;
+		}
+
+		vet[j+1] = temp;
+	}
+}
+
