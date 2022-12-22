e126f799969a7f04f5eae1095ea57b05bd4029ca
Paula <paulactalim@gmail.com>
2022-12-22 13:46:15 -0300

    criacao de arquivo para ordenacao em cpp

---
+++ Mergesort/cpp/mergeCharDecrescente.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+Descricao: apartir de um vetor de caracteres, essa 
+funcao subdivide o vetor em outros dois e intercala 
+seus elementos entre si no vetor principal
+
+Parametros: um vetor de caracteres (vetor a ser intercalado) e 
+3 inteiros (o primeiro, o ultimo e o indice do elemento do 
+meio do vetor)
+*/
+void intercalar (char vet[], int esq, int meio, int dir) {
+	int nEsq = (meio - esq) + 1;
+	int nDir = dir - meio;
+
+	char arrayEsq[nEsq+1];
+	char arrayDir[nDir+1];
+
+	//Sentinela no final dos dois arrays
+	arrayEsq[nEsq] = -0x7FFFFFFF;
+	arrayDir[nDir] = -0x7FFFFFFF;
+
+	int iEsq, iDir, i;
+
+	//Inicializar primeiro subarray
+	for (iEsq = 0; iEsq < nEsq; iEsq++){
+		arrayEsq[iEsq] = vet[esq+iEsq];
+	}
+
+	//Inicializar segundo subarray
+	for (iDir = 0; iDir < nDir; iDir++){
+		arrayDir[iDir] = vet[(meio + 1) + iDir];
+	}
+
+	//Intercalacao propriamente dita
+	for (iEsq = 0, iDir = 0, i = esq; i <= dir; i++){
+		if (arrayEsq[iEsq] >= arrayDir[iDir]) {
+			vet[i] = arrayEsq[iEsq];
+			iEsq++;
+		} else {
+			vet[i] = arrayDir[iDir];
+			iDir++;
+		}
+	}
+}
+
+/* 
+Descricao: essa funcao ordena um vetor de caracteres 
+em ordem descrescente com o metodo mergesort
+
+Parametro: um vetor de caracteres (vetor a ser ordenado)
+e dois inteiros (indice do primeiro e do ultimo elemento do vetor)
+*/
+void merge (char vet[], int esq, int dir) {
+	if (esq < dir) {
+		int meio = (esq + dir)/2;
+		merge(vet, esq, meio);
+		merge(vet, meio + 1, dir);
+		intercalar(vet, esq, meio, dir);
+	}
+}
+
+/*
+Descricao: essa funcao chama pela funcao merge 
+para ordena o vetor em ordem decrescente, possuindo 
+argumentos mais simplificados
+
+Parametro: um vetor de caracteres (vetor a ser ordenado)
+e um inteiro (tamanho do vetor)
+*/
+void mergesort (char vet[], int n) {
+	merge (vet, 0, n-1);
+}
