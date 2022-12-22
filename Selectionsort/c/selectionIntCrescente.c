b7a88d8f37f9e0cef2a084df4bdfde17ed856e34
Paula <paulactalim@gmail.com>
2022-12-22 19:38:33 -0300

    traducao do codigo

--- Selectionsort/c/selectionIntCrescente.c
+++ Selectionsort/c/selectionIntCrescente.c
@@ -1,35 +1,37 @@
-public class selectionIntCrescente {
-	/*
-	* Descricao: essa funcao troca dois elementos de um vetor de inteiros
-	* Parametros: um vetor de inteiros (vetor que tera os 
-	* elementos trocados) e dois inteiros (indice dos 
-	* dois elementos a serem trocados)
-	*/
-	public static void swap (int[] vet, int i, int j) {
-		int aux = vet[i];
-		vet[i] = vet[j];
-		vet[j] = aux;
-	}
+#include <stdio.h>
+#include <stdlib.h>
 
-	/* 
-	* Descricao: essa funcao ordena um vetor de inteiros
-	* em ordem crescente com o metodo selectionsort
-	* 
-	* Parametro: um vetor de inteiros (vetor a ser ordenado)
-	*/
-	public static void selectionsort (int vet[]) {
-		for (int i = 0; i < vet.length-1; i ++) {
-			int menor = i;
+/*
+Descricao: essa funcao troca dois elementos de um vetor de inteiros
+Parametros: um vetor de inteiros (vetor que tera os 
+elementos trocados) e dois inteiros (indice dos 
+dois elementos a serem trocados)
+*/
+void swap (int vet[], int i, int j) {
+	int aux = vet[i];
+	vet[i] = vet[j];
+	vet[j] = aux;
+}
 
-			for (int j = i + 1; j < vet.length; j++) {
-				if (vet[menor] > vet[j]) {
-					menor = j;
-				}
+/* 
+Descricao: essa funcao ordena um vetor de inteiros
+em ordem crescente com o metodo selectionsort
+
+Parametro: um vetor de inteiros (vetor a ser ordenado)
+e um inteiro (tamanho do vetor)
+*/
+void selectionsort (int vet[], int n) {
+	for (int i = 0; i < n-1; i ++) {
+		int menor = i;
+
+		for (int j = i + 1; j < n; j++) {
+			if (vet[menor] > vet[j]) {
+				menor = j;
 			}
+		}
 
-			if (i != menor) {
-				swap(vet, i, menor);
-			}
+		if (i != menor) {
+			swap(vet, i, menor);
 		}
 	}
 }
