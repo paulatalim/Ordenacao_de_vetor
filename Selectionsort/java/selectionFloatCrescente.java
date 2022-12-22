41933d862b61a1a6e0346a48487552fb9fa35e01
Paula <paulactalim@gmail.com>
2022-12-22 15:46:36 -0300

    adaptacao dos codigos

--- Selectionsort/java/selectionFloatCrescente.java
+++ Selectionsort/java/selectionFloatCrescente.java
@@ -1,23 +1,23 @@
-public class selectionIntCrescente {
+public class selectionFloatCrescente {
     /*
-	 * Descricao: essa funcao troca dois elementos de um vetor de inteiros
-	 * Parametros: um vetor de inteiros (vetor que tera os 
+	 * Descricao: essa funcao troca dois elementos de um vetor de reais
+	 * Parametros: um vetor de reais (vetor que tera os 
 	 * elementos trocados) e dois inteiros (indice dos 
 	 * dois elementos a serem trocados)
 	 */
-	public static void swap (int[] vet, int i, int j) {
-		int aux = vet[i];
+	public static void swap (float[] vet, int i, int j) {
+		float aux = vet[i];
 		vet[i] = vet[j];
 		vet[j] = aux;
 	}
     
     /* 
-	 * Descricao: essa funcao ordena um vetor de inteiros
+	 * Descricao: essa funcao ordena um vetor de reais
 	 * em ordem crescente com o metodo selectionsort
 	 * 
-	 * Parametro: um vetor de inteiros (vetor a ser ordenado)
+	 * Parametro: um vetor de reais (vetor a ser ordenado)
 	 */
-	public static void selectionsort (int vet[]) {
+	public static void selectionsort (float[] vet) {
 		for (int i = 0; i < vet.length-1; i ++) {
 			int menor = i;
 
