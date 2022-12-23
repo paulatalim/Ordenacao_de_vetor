6943fc61909dc509cc6499e6c731c029c7f3d1ab
Paula <paulactalim@gmail.com>
2022-12-23 16:53:55 -0300

    criacao de arquivos cpp

---
+++ Quicksort/cpp/quickCharCrescente.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Descricao: essa funcao troca dois elementos de um vetor de caracteres
+Parametros: um vetor de caracteres (vetor que tera os 
+elementos trocados) e dois inteiros (indice dos 
+dois elementos a serem trocados)
+*/
+void swap (char vet[], int i, int j) {
+    char aux = vet[i];
+    vet[i] = vet[j];
+    vet[j] = aux;
+}
+
+/* 
+Descricao: esse procedimento ordena um vetor de caracteres 
+em ordem alfabetica com o metodo quicksort
+
+Parametro: um vetor de caracteres (vetor a ser ordenado) 
+e dois inteiros (indice do primeiro e do ultimo elemento do vetor)
+*/
+void quick (char vet[], int esq, int dir) {
+    int i, j, posicao;
+    char pivo;
+
+    i = esq;
+    j = dir;
+    posicao = (esq + dir) / 2;
+    pivo = vet[posicao];
+
+    while (i <= j) {
+        while (vet[i] < pivo) {
+            i ++;
+        }
+
+        while (vet[j] > pivo) {
+            j --;
+        }
+
+        if (i <= j) {
+            //Troca dos valores no vetor
+            swap(vet, i, j);
+            i++;
+            j--;
+        }
+    }
+    
+    if (esq < j) {
+        quick (vet, esq, j);
+    }
+
+    if (i < dir) {
+        quick (vet, i, dir);
+    }
+}
+
+/*
+Descricao: esse procedimento chama pelo procedimento quick 
+para ordena o vetor em ordem alfabetica, possuindo 
+argumentos mais simplificados
+
+Parametro: um vetor de caracteres (vetor a ser ordenado) 
+e um inteiro (quantidade de numeros validos dentro do vetor)
+*/
+void quicksort (char vet[], int n) {
+    quick(vet, 0, n-1);
+}
