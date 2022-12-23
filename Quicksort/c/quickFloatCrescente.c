54076d4a4c151ed6efe31656125bbcfcbd9e5161
Paula <paulactalim@gmail.com>
2022-12-23 16:42:10 -0300

    criacao de arquivos em c

---
+++ Quicksort/c/quickFloatCrescente.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Descricao: essa funcao troca dois elementos de um vetor de inteiros
+Parametros: um vetor de inteiros (vetor que tera os 
+elementos trocados) e dois inteiros (indice dos 
+dois elementos a serem trocados)
+*/
+void swap (int vet[], int i, int j) {
+    int aux = vet[i];
+    vet[i] = vet[j];
+    vet[j] = aux;
+}
+
+/* 
+Descricao: esse procedimento ordena um vetor de inteiros 
+em ordem crescente com o metodo quicksort
+
+Parametro: um vetor de inteiros (vetor a ser ordenado) 
+e dois inteiros (indice do primeiro e do ultimo elemento do vetor)
+*/
+void quick (int vet[], int esq, int dir) {
+    int i, j, posicao;
+    int pivo;
+    i = esq;
+    j = dir;
+    posicao = (esq + dir) / 2;
+    pivo = vet[posicao];
+
+    while (i <= j) {
+        while (vet[i] < pivo) {
+            i ++;
+        }
+
+        while (vet[j] > pivo) {
+            j --;
+        }
+
+        if (i <= j) {
+            //Troca dos valores no vetor
+            swap(vet, i, j);
+            i++;
+            j--;
+        }
+    }
+    
+    if (esq < j) {
+        quick (vet, esq, j);
+    }
+
+    if (i < dir) {
+        quick (vet, i, dir);
+    }
+}
+
+/*
+Descricao: esse procedimento chama pelo procedimento quick 
+para ordena o vetor em ordem crescente, possuindo 
+argumentos mais simplificados
+
+Parametro: um vetor de inteiros (vetor a ser ordenado) 
+e um inteiro (quantidade de numeros validos dentro do vetor)
+*/
+void quicksort (int vet[], int n) {
+    quick(vet, 0, n-1);
+}
