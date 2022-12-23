e974392324d8ff580816ffce818c6d9323286768
Paula <paulactalim@gmail.com>
2022-12-23 12:20:15 -0300

    criacao de arquivos cpp

---
+++ Selectionsort/cpp/selectionDoubleCrescente.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Descricao: essa funcao troca dois elementos de um vetor de double
+Parametros: um vetor de double (vetor que tera os 
+elementos trocados) e dois inteiros (indice dos 
+dois elementos a serem trocados)
+*/
+void swap (double vet[], int i, int j) {
+	double aux = vet[i];
+	vet[i] = vet[j];
+	vet[j] = aux;
+}
+
+/* 
+Descricao: essa funcao ordena um vetor de double
+em ordem crescente com o metodo selectionsort
+
+Parametro: um vetor de double (vetor a ser ordenado)
+e um inteiro (tamanho do vetor)
+*/
+void selectionsort (double vet[], int n) {
+	for (int i = 0; i < n-1; i ++) {
+		int menor = i;
+
+		for (int j = i + 1; j < n; j++) {
+			if (vet[menor] > vet[j]) {
+				menor = j;
+			}
+		}
+
+		if (i != menor) {
+			swap(vet, i, menor);
+		}
+	}
+}
