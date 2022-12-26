43db7c8e8fddb364f11cb23d4aca2996a044d2c8
Paula <paulactalim@gmail.com>
2022-12-26 14:13:59 -0300

    adaptacao do codigo

--- Selectionsort/cpp/selectionStringCrescente.cpp
+++ Selectionsort/cpp/selectionStringCrescente.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 
+using namespace std;
+
 /*
-Descricao: essa funcao troca dois elementos de um vetor de inteiros
-Parametros: um vetor de inteiros (vetor que tera os 
+Descricao: essa funcao troca dois elementos de um vetor de string
+Parametros: um vetor de string (vetor que tera os 
 elementos trocados) e dois inteiros (indice dos 
 dois elementos a serem trocados)
 */
-void swap (int vet[], int i, int j) {
-	int aux = vet[i];
+void swap (string vet[], int i, int j) {
+	string aux = vet[i];
 	vet[i] = vet[j];
 	vet[j] = aux;
 }
 
 /* 
-Descricao: essa funcao ordena um vetor de inteiros
-em ordem crescente com o metodo selectionsort
+Descricao: essa funcao ordena um vetor de string
+em ordem alfabetica com o metodo selectionsort
 
-Parametro: um vetor de inteiros (vetor a ser ordenado)
-e um inteiro (tamanho do vetor)
+Parametro: um vetor de string (vetor a ser ordenado)
 */
-void selectionsort (int vet[], int n) {
-	for (int i = 0; i < n-1; i ++) {
+void selectionsort (string vet[]) {
+	for (int i = 0; i < vet->length()-1; i ++) {
 		int menor = i;
 
-		for (int j = i + 1; j < n; j++) {
-			if (vet[menor] > vet[j]) {
+		for (int j = i + 1; j < vet->length(); j++) {
+			if (vet[menor].compare(vet[j]) > 0) {
 				menor = j;
 			}
 		}
