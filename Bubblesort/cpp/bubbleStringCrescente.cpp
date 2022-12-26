6f4045077d201bd570fc395aec024fc116cfdc66
Paula <paulactalim@gmail.com>
2022-12-26 13:41:41 -0300

    adaptacoa do codigos

--- Bubblesort/cpp/bubbleStringCrescente.cpp
+++ Bubblesort/cpp/bubbleStringCrescente.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 /*
-Descricao: esse procedimento troca dois elementos de um vetor de inteiros
+Descricao: esse procedimento troca dois elementos de um vetor de string
 
-Parametros: um vetor de inteiros (vetor que tera os 
+Parametros: um vetor de string (vetor que tera os 
 elementos trocados) e dois inteiros (indice dos 
 dois elementos a serem trocados)
 */
@@ -16,14 +16,14 @@ void swap (string vet[], int i, int j) {
 }
 
 /* 
-Descricao: esse procedimento ordena um vetor de inteiros
+Descricao: esse procedimento ordena um vetor de string
 em ordem crescente com o metodo bubblesort
 
-Parametro: um vetor de inteiros (vetor a ser ordenado) 
+Parametro: um vetor de string (vetor a ser ordenado) 
 e um inteiro (tamanho do vetor)
 */
-void bubblesort (string vet[], int n) {
-    for (int i = n-1; i > 0; i--) {
+void bubblesort (string vet[]) {
+    for (int i = vet->length() - 1; i > 0; i--) {
         for (int j = 0; j < i; j++) {
             if (vet[j].compare(vet[j+1]) > 0) {
                 swap(vet, j, j+1);
